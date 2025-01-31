355490cd0ee5d31f74477103b2a64800d9bab123
Wictor Rindebrant <wrindebrant@gmail.com>
2025-01-31 22:03:31 +0100

    Fixed game side

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-document.getElementById('new-game').addEventListener('click', () => {
-    alert('Started a new game!');
-});
-
 function navigateToPage(page) {
     window.location.href = page;
 }
+
+// document.getElementById('new-game').addEventListener('click', () => {
+//     alert('Started a new game!');
+// });
